2f77e4e98fb2537ca8e2d6ce2da53d23c40f2078
Jagadeesh <raavijagadeesh5@gmail.com>
2026-06-27 19:57:24 +0530

    BINARY TO DECIMAL CONVERSION

---
+++ Binary-decimal.py
@@ -0,0 +1,14 @@
+# binary=input("enter binary number:")
+# decimal=int(binary,2)
+# print("Decimal =",decimal) 
+#-----------------------------------#
+binary=input("enter binary number:")
+decimal=0
+power=0
+for digit in binary[::-1]:
+    decimal+=int(digit) * (2 ** power)
+    power+=1
+print("Decimal =", decimal)
+
+
+
